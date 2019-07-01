1b046c94edd4f0d986d6f51cd9fbb97c65d5e41a
Timur Melnikov <melnikovt@gmail.com>
2019-07-01 10:14:47 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,32 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', 'PostController')->names('blog-posts');
+});
+
+
+// Админка блога
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog'
+];
+
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index', 'edit', 'update', 'create', 'store'];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+
+    //BlogPost
+    Route::resource('posts', 'PostController')
+        ->except(['show'])
+        ->names('blog.admin.posts');
+});
+
+//Route::resource('rest', 'RestTestController')->names('restTest');
